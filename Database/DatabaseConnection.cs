d188ba33862b5576a5be17a52f2ea58f1336b0b8
DinoDany17 <minorisan22@gmail.com>
2025-01-11 17:52:12 -0800

    .env and SQLite connection

---
+++ Database/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SQLite;
+
+namespace GestorDeConsumo
+{
+    public class DatabaseConnection
+    {
+        private static string directorio = Environment.GetEnvironmentVariable("APP_DIRECTORY") ?? "";
+        private static string databaseLocation = Path.Combine(directorio, "Database", "master.db");
+        private static string connectionString = $"Data Source={databaseLocation};Version=3;";
+
+        public string GetDatabaseLocation()
+        {
+            return databaseLocation;
+        }
+
+        public void CreateDatabase()
+        {
+            SQLiteConnection.CreateFile(databaseLocation);
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using dotenv.net;
+
 namespace GestorDeConsumo
 {
     internal static class Program
@@ -8,10 +10,27 @@ namespace GestorDeConsumo
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new HomePage());
+            //Get the current directory of the application
+            string appDirectory = Directory.GetCurrentDirectory();
+            string cleanAppDirectory = appDirectory.Split("bin")[0];
+
+            //Create the .env file if it does not exist and read the environment variables
+            string envPath = Path.Combine(cleanAppDirectory, ".env");
+            if (!File.Exists(envPath))
+            {
+                File.WriteAllText(envPath, "APP_DIRECTORY=" + cleanAppDirectory);
+            }
+            DotEnv.Load(new DotEnvOptions(envFilePaths: new List<String> { envPath }));
+
+            //Create the database if it does not exist
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            if (!File.Exists(databaseConnection.GetDatabaseLocation()))
+            {
+                databaseConnection.CreateDatabase();
+            }
+
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new HomePage());
         }
     }
 }
